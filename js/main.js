ff3fcdd6508de8c04e594a08c35f6c8fa673444c
anonymous-cayber <anonymous.cayber@gmail.com>
2020-08-05 03:01:36 +0300

    add js

--- js/main.js
+++ js/main.js
@@ -31,4 +31,14 @@ $(window).load(function () {
     return false;
   });
   $('input[type="tel"]').mask("+7 (999) 999-99-99");
+  // показывать карты только тогда докрутили до неё
+  var reviews = $('.reviews');
+  var reviewsTop = reviews.offset().top;
+  $(window).bind('scroll', function(){
+    var windowTop = $(this).scrollTop();
+    if (windowTop > reviewsTop) {
+      $('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Af7da640d6a19b561081df03f4ca5b107fca087e993eb25bf2245ee37d171686f&amp;width=100%25&amp;height=410&amp;lang=ru_RU&amp;scroll=false" ></script>')
+      $(window).unbind('scroll')
+    }
+  });
 });
