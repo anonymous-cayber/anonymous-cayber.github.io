e9fca32d4c6dbdd71879a0d3bfc6c601deec9d54
anonymous-cayber <anonymous.cayber@gmail.com>
2020-08-05 21:09:47 +0300

    php-in

---
+++ optipic.io/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Config of OptiPic.io plugin
+ * 
+ * @author https://optipic.io/
+ * @copyright (c) 2018, https://optipic.io
+ */
+
+$optipicCfg = array(
+    "secretkey" => "LSvBSxjJqFbqkEmWwdBN3bEx7yqZWnVlP2953vyBr9X1tFpWziyauaL3bLY2Yi4jbaMgJUkfQ5o_v4IK-QzcsFq8BnVCQ2PZExRoqMZk-KBTm-XSQLQzdFeZwLLd9tnx-0te5rizrkukdtAXdbJVgRo8u1bSbXMQlZpTpIco9l5K2JgC5voviQvlfsWBG5I1A4r-0cBN-0zJbmvKCWwHNtB4qHvLFQ0zRKh98-cmkkZDS-F4TBUIV8LqMtJAb2B",
+    "api_url" => "https://optipic.io/api/",
+);
+?>
